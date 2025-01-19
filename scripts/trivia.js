b2bc2212839585c474b493ffb86105d93945e768
t-p-r <quanthangthien1000@gmail.com>
2025-01-18 18:05:52 -0800

    more trivias

--- scripts/trivia.js
+++ scripts/trivia.js
@@ -3,11 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
         const dailyUsage = data.dailyUsage || { prompts: 0, energyUsed: 0 };
         const weeklyUsage = data.weeklyUsage || { prompts: 0, energyUsed: 0 };
 
+        function f0() { return "That is enough to charge " + dailyUsage.energyUsed / 16 + " phone to 100%"; }
+        function f1() { return "That is enough to light a LED bulb for " + dailyUsage.energyUsed / 10 + " hours"; }
+        function f2() { return "That is the equivalent of a hamster running on a wheel for " + dailyUsage.energyUsed * 8 + " days!"; }
+        function f3() {
+            return "That is the daily energy intake of " + dailyUsage.energyUsed / 2.5 + " people!";
+        }
 
-        function f0() { return "That is enough to charge " + dailyUsage.prompts / 16 + " phone to 100%"; }
-        function f1() { return "That is enough to light a LED bulb for " + dailyUsage.prompts / 10 + " hours"; }
-
-        func_list = [f0, f1]
+        func_list = [f0, f1, f2, f3]
 
         const Trivia = document.getElementById('trivia');
         Trivia.textContent = func_list[Math.floor(Math.random() * func_list.length)]();
