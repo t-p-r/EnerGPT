7acce0adbe76fcb8204be96ca687a8d79a0729e3
t-p-r <quanthangthien1000@gmail.com>
2025-01-18 19:10:23 -0800

    add water trivia

--- scripts/trivia.js
+++ scripts/trivia.js
@@ -1,23 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     chrome.storage.local.get(['dailyUsage', 'weeklyUsage'], (data) => {
-        const dailyUsage = data.dailyUsage || { prompts: 0, energyUsed: 0 };
+        const dailyUsage = data.dailyUsage || { prompts: 0, energyUsed: 0, waterUsed: 0 };
 
-        function f0() { return "That is enough to charge " + Math.round(dailyUsage.energyUsed * 6.25) / 100 + " phones to 100%."; }
-        function f1() { return "That is enough to light a LED bulb for " + Math.round(dailyUsage.energyUsed * 10) / 100 + " hours."; }
+        function f0() { return "The energy is enough to charge " + Math.round(dailyUsage.energyUsed * 6.25) / 100 + " phones to 100%."; }
+        function f1() { return "The energy is enough to light a LED bulb for " + Math.round(dailyUsage.energyUsed * 10) / 100 + " hours."; }
         function f2() {
-            return "That is equivalent to a hamster running on a wheel for " + Math.round(dailyUsage.energyUsed * 800) / 100 + " days!";
+            return "The energy is equivalent to a hamster running on a wheel for " + Math.round(dailyUsage.energyUsed * 800) / 100 + " days!";
         }
         function f3() {
-            return "That is the daily energy intake of " + Math.round(dailyUsage.energyUsed * 40) / 100 + " people!";
+            return "The energy is the daily energy intake of " + Math.round(dailyUsage.energyUsed * 40) / 100 + " people!";
         }
         function f4() {
-            return "That is like a dog chasing its tail for " + Math.round(dailyUsage.energyUsed * 120) / 100 + "minutes!";
+            return "The energy is like a dog chasing its tail for " + Math.round(dailyUsage.energyUsed * 120) / 100 + "minutes!";
         }
         function f5() {
-            return "That is enough to brew " + Math.round(dailyUsage.energyUsed / 60 * 100) / 100 + " cups of coffee!";
+            return "The energy is enough to brew " + Math.round(dailyUsage.energyUsed / 60 * 100) / 100 + " cups of coffee!";
         }
         function f6() {
-            return "That is enough for Google to send " + Math.round(dailyUsage.energyUsed * 100) + " emails!";
+            return "The energy is enough for Google to send " + Math.round(dailyUsage.energyUsed * 100) + " emails!";
+        }
+        function f7() {
+            return "The water is enough for you to brush your teeth" + Math.round(dailyUsage.waterUsed * 2) / 100 + " emails!";
         }
 
         func_list = [f0, f1, f2, f3, f4, f5, f6];
